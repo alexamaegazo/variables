21616ae93498a109b267c196d6111f67a89aa1f9
alexamaegazo <alexamaegazo5@gmail.com>
2024-09-13 00:01:47 +0800

    Initial commit with viriable.js

---
+++ variable.js
@@ -0,0 +1,11 @@
+let name = "Alexa Mae Gazo";
+console.log('My name is '  + name);
+
+let whatDoYouWannaBeInYourLife = "Programmer";
+console.log("What do you wanna become in your life:", whatDoYouWannaBeInYourLife );
+
+let gender = "Female"
+console.log("gender:", gender);
+
+let twitterHandle = "alexa2061"
+console.log("Twitter Handle:", twitterHandle);
